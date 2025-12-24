a8b632f0805d85a403cca1a2e4d34507973e1304
ShubhamSingh3102 <shubham18singh07@gmail.com>
2025-12-24 10:15:51 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (9.76%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        int prev2 = 1, prev = 1;
+        for (int i = 2; i <= n; i++) {
+            int cur = prev + prev2;
+            prev2 = prev;
+            prev = cur;
+        }
+        return prev;
+    }
+}
