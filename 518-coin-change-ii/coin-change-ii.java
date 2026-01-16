636a83282771b5e62bdd5570b95efc1c43ef3c7c
ShubhamSingh3102 <shubham18singh07@gmail.com>
2026-01-16 23:16:41 +0530

    Time: 11 ms (62.91%) | Memory: 57.1 MB (29.23%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+    int[][] dp = new int[n][amount + 1];
+
+    // Base case: target 0 → 1 way (pick nothing)
+    for (int i = 0; i < n; i++) {
+        dp[i][0] = 1;
+    }
+
+    // Base case: using only coin[0]
+    for (int t = 0; t <= amount; t++) {
+        if (t % coins[0] == 0) {
+            dp[0][t] = 1;
+        }
+    }
+
+    for (int i = 1; i < n; i++) {
+        for (int t = 1; t <= amount; t++) {
+
+            int notTake = dp[i - 1][t];
+            int take = 0;
+
+            if (coins[i] <= t) {
+                take = dp[i][t - coins[i]]; // unbounded
+            }
+
+            dp[i][t] = notTake + take;
+        }
+    }
+
+    return dp[n - 1][amount];
+    }
+}
