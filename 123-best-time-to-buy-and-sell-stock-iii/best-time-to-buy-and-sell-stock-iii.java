0d751b96b3067f1556f56ac0cf27170487da0fb9
ShubhamSingh3102 <shubham18singh07@gmail.com>
2026-02-16 20:36:23 +0530

    Time: 78 ms (28.82%) | Memory: 80 MB (57.54%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -2,38 +2,32 @@ class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
 
-       int[][][] dp = new int[n+1][2][3];
+        // after and cur arrays (like screenshot)
+        int[][] after = new int[2][3];
+        int[][] cur = new int[2][3];
 
-       // cap == 0
-       for(int ind = 0; ind < n; ind++ ){
-           for(int buy = 0 ; buy <= 1; buy++){
-               dp[ind][buy][0] = 0;
-           }
-       }
 
-       // ind == n
-       for(int buy = 0 ; buy <= 1; buy++){
-           for(int cap = 0 ; cap <= 2; cap++){
-               dp[n][buy][cap] = 0;
-           }
-       }
-       int profit;
+        int profit;
 
-       for(int ind = n-1 ; ind >= 0; ind--){
-           for(int buy = 0 ; buy <= 1; buy++){
-               for(int cap = 1; cap <= 2; cap++){
-                   if(buy == 1) {
-                       int take = -prices[ind] + dp[ind+1][0][cap];
-                       int notTake = dp[ind+1][1][cap];
-                       dp[ind][buy][cap] = Math.max(take, notTake);
-                   }else { // sell
-                       int take = +prices[ind] + dp[ind+1][1][cap-1];
-                       int notTake = dp[ind+1][0][cap];
-                       dp[ind][buy][cap]  = Math.max(take, notTake);
-                   }
-               }
-           }
-       }
-       return dp[0][1][2];
+        for(int ind = n-1 ; ind >= 0; ind--){
+            for(int buy = 0 ; buy <= 1; buy++){
+                for(int cap = 1; cap <= 2; cap++){
+                    if(buy == 1) {
+                        int take = -prices[ind] + after[0][cap];
+                        int notTake = after[1][cap];
+                        cur[buy][cap] = Math.max(take, notTake);
+                    }else { // sell
+                        int take = +prices[ind] + after[1][cap-1];
+                        int notTake = after[0][cap];
+                        cur[buy][cap]  = Math.max(take, notTake);
+                    }
+                }
+            }
+            // move cur → after....
+            for (int i = 0; i < 2; i++) {
+                after[i] = cur[i].clone();
+            }
+        }
+        return after[1][2];
     }
 }
