027e3feb3396fdbe1b665224edf51fef81e0ccad
ShubhamSingh3102 <shubham18singh07@gmail.com>
2026-03-12 22:28:44 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (97.69%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -3,10 +3,17 @@ class Solution {
         // TC ---> O(log2n)
         // SC ---> O(1)
         // find mask
-        int mask = 1;
-        while (mask < n) {
-            mask = (mask << 1) | 1; // first left shift and then or with 1....
+        // int mask = 1;
+        // while (mask < n) {
+        //     mask = (mask << 1) | 1; // first left shift and then or with 1....
+        // }
+        // return mask ^ n;
+        if(n == 0) {
+            return 1;
         }
+        // find number of bit required to represent the number n...
+        int bits = (int)(Math.floor(Math.log(n) / Math.log(2))) +1;
+        int mask = (int) (Math.pow(2,bits) - 1);
         return mask ^ n;
     }
 }
