f4107716603d5b8d539c164903f3a3b372ee8ac0
ShubhamSingh3102 <shubham18singh07@gmail.com>
2026-06-16 12:29:15 +0530

    Time: 1 ms (54.08%) | Memory: 43.4 MB (65.57%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,17 +1,7 @@
 class Solution {
-    public void find(String digits,int idx,StringBuilder diary,List<String> res){
+    public void find(String digits,int idx,StringBuilder diary,List<String> res, HashMap<Character,String> map){
 
         int n = digits.length();
-        HashMap<Character,String> map = new HashMap<>();
-
-        map.put('2', "abc");
-        map.put('3', "def");
-        map.put('4', "ghi");
-        map.put('5', "jkl");
-        map.put('6', "mno");
-        map.put('7', "pqrs");
-        map.put('8', "tuv");
-        map.put('9', "wxyz");
 
         if(idx == n){
             res.add(String.valueOf(diary));
@@ -23,17 +13,28 @@ class Solution {
 
         for(int j = 0; j < choice.length(); j++){
             diary.append(choice.charAt(j));
-            find(digits,idx + 1,diary,res);
+            find(digits,idx + 1,diary,res,map);
             diary.deleteCharAt(diary.length() - 1);
         }
         return;
     }
     public List<String> letterCombinations(String digits) {
+        HashMap<Character,String> map = new HashMap<>();
+
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
+
         StringBuilder diary = new StringBuilder();
         List<String> res = new ArrayList<>();
         int n = digits.length();
 
-        find(digits,0,diary,res);
+        find(digits,0,diary,res,map);
         return res;
     }
 }
