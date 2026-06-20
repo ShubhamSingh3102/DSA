daef48ea3cf1bccb866bc57462b5f27011300d13
ShubhamSingh3102 <shubham18singh07@gmail.com>
2026-06-20 14:14:14 +0530

    Time: 1 ms (100.00%) | Memory: 47.5 MB (42.35%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -5,15 +5,18 @@ class Solution {
         int n = nums.length;
         int[] res = new int[n];
 
+//      Sabse bada square hamesha -->   left end ya right end
+
         int left = 0;
         int right = n - 1;
         int k = n - 1;
 
-        while (left <= right) {
+        while(left <= right){
             int l = nums[left] * nums[left];
             int r = nums[right] * nums[right];
 
-            if (l > r) {
+            // 16 > 100 ??
+            if(l > r){
                 res[k--] = l;
                 left++;
             } else {
