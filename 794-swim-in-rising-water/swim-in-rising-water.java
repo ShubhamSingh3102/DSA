e923b0b503d6cfe09732b17eebf90b99b5ad5756
ShubhamSingh3102 <shubham18singh07@gmail.com>
2026-08-08 23:22:02 +0530

    Time: 17 ms (14.00%) | Memory: 46.4 MB (45.86%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.java
+++ 794-swim-in-rising-water/swim-in-rising-water.java
@@ -1,13 +1,18 @@
 class Solution {
-    public class Pair{
-        int first;
-        int second;
+    public class Pair {
 
-        Pair(int first, int second){
-            this.first = first;
-            this.second = second;
+        int diff;
+        int row;
+        int col;
+
+        Pair(int diff, int row, int col) {
+            this.diff = diff;
+            this.row = row;
+            this.col = col;
         }
     }
+    static int[] x = {1,-1,0,0};
+    static int[] y = {0,0,1,-1};
 
     public boolean isValid(int i, int j, int n, int m){
         if(i < 0 || i >= n || j < 0 || j >= m){
@@ -15,81 +20,64 @@ class Solution {
         }
         return true;
     }
-
-    static int[] x = {1,-1,0,0};
-    static int[] y = {0,0,1,-1};
-
-    public boolean bfs(int[][] grid,int n,int m, int money){
-
-        if(grid[0][0] > money){
-            return false;
-        }
-
-        Queue<Pair> q = new LinkedList<>();
-
-        ArrayList<ArrayList<Integer>> visited = new ArrayList<>();
-
-        for(int i = 0; i < n; i++){
-            visited.add(new ArrayList<>());
-
-            for(int j = 0; j < m; j++){
-                visited.get(i).add(0);
-            }
-        }
-
-        q.add(new Pair(0,0));
-
-        visited.get(0).set(0,1);
-
-        while (!q.isEmpty()){
-            Pair p = q.poll();
-
-            int row = p.first;
-            int col = p.second;
-
-            if(row == n - 1 && col == m - 1){
-                return true;
-            }
-
-            for(int i = 0; i < 4; i++){
-                int r = row + x[i];
-                int c = col + y[i];
-
-                if(isValid(r,c,n,m) && visited.get(r).get(c) == 0 && money >= grid[r][c]){
-                    q.add(new Pair(r,c));
-
-                    visited.get(r).set(c,1);
-                }
-            }
-        }
-        return false;
-    }
     public int swimInWater(int[][] grid) {
+        // max absolute Diff....
         int n = grid.length;
         int m = grid[0].length;
 
-        int low = grid[0][0];
-
-        int high = Integer.MIN_VALUE;
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
 
         for(int i = 0; i < n; i++){
+            res.add(new ArrayList<>());
+
             for(int j = 0; j < m; j++){
-                high = Math.max(high,grid[i][j]);
+                res.get(i).add(Integer.MAX_VALUE);
             }
         }
 
-        int res = 0;
-        while (low <= high){
-            int mid = low + (high - low) / 2;
+        PriorityQueue<Pair> pq = new PriorityQueue<>(
+                (a, b) -> Integer.compare(a.diff, b.diff)
+        );
 
-            if(bfs(grid,n,m,mid)){
-                res = mid;
+        // initially source ka distance zero h...
+        res.get(0).set(0,grid[0][0]);
 
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+        // (money, {x,y})
+        pq.add(new Pair(grid[0][0],0,0));
+
+        while (!pq.isEmpty()){
+            Pair p = pq.poll();
+
+            int money = p.diff;
+            int row = p.row;
+            int col = p.col;
+
+            if(row == n - 1 && col == m - 1){
+                return money;
+            }
+            // Stale Entry
+            if(money > res.get(row).get(col)){
+                continue;
+            }
+
+            for(int k = 0; k < 4; k++){
+                int r = row + x[k];
+                int c = col + y[k];
+
+                if(!isValid(r,c,n,m)){
+                    continue;
+                }
+
+                int newMoney = Math.max(money,grid[r][c]);
+
+                // relaxation
+                if(newMoney < res.get(r).get(c)){
+                    res.get(r).set(c,newMoney);
+
+                    pq.add(new Pair(newMoney,r,c));
+                }
             }
         }
-        return res;
+        return res.get(n-1).get(m-1);
     }
 }
