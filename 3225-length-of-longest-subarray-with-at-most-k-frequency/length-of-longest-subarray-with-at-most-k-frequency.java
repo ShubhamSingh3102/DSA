530ab83bd2bfe5dd8fb7ecc50056853d68eeb174
ShubhamSingh3102 <shubham18singh07@gmail.com>
2026-08-12 23:09:49 +0530

    Time: 81 ms (9.12%) | Memory: 88.8 MB (74.81%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
@@ -1,33 +1,28 @@
 class Solution {
     public int maxSubarrayLength(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
 
         int low = 0;
         int high = 0;
         int res = Integer.MIN_VALUE;
-        int culprit = 0;
 
-        for (high = 0; high < nums.length; high++) {
+
+        for(high = 0; high < nums.length; high++){
 
             // Expand window....
             map.put(nums[high], map.getOrDefault(nums[high], 0) + 1);
 
-            if (map.get(nums[high]) == k + 1) {
-                culprit++;
-            }
+            // Shrink Window....
+            while (low < high && map.get(nums[high]) > k){
+               map.put(nums[low], map.get(nums[low]) - 1);
 
-            if (culprit > 0) {
-                map.put(nums[low], map.get(nums[low]) - 1);
-                if (map.get(nums[low]) == k) {
-                    culprit--;
+                if(map.get(nums[low]) == 0){
+                    map.remove(nums[low]);
                 }
                 low++;
             }
 
-            // valid subArray...
-            if (culprit == 0) {
-                res = Math.max(res, high - low + 1);
-            }
+            res = Math.max(res, high - low + 1);
         }
         return res;
     }
