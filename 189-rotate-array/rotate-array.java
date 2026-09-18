4c04e77d187cf43cb6ed4bf06bd93049892691f5
ShubhamSingh3102 <shubham18singh07@gmail.com>
2026-09-18 19:05:10 +0530

    Time: 7 ms (9.28%) | Memory: 273.3 MB (5.38%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,32 +1,16 @@
 class Solution {
-    public void reverseSection(int[] nums, int start, int end){
-
-        // reverse whole group
-        while (start < end){
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-
-            start++;
-            end--;
-        }
-    }
     public void rotate(int[] nums, int k) {
         int n = nums.length;
-
         k = k % n;
 
-        if (k == 0){
-            return;
+        int[] result = new int[n];
+
+        for(int i = 0; i < n; i++){
+            result[(i + k) % n] = nums[i];
         }
 
-        // reverse first k element
-        reverseSection(nums,0,n-k-1);
-
-        // reverse remaining n - k element
-        reverseSection(nums, n-k, n-1);
-
-        // reverse whole array
-        reverseSection(nums,0, n-1);
+        for(int i = 0; i < n; i++){
+            nums[i] = result[i];
+        }
     }
 }
