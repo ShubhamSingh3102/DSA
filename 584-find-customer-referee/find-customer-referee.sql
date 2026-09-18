764b52ed546f9c70ec7c8ab98df225abb712cd9d
ShubhamSingh3102 <shubham18singh07@gmail.com>
2026-09-18 23:22:39 +0530

    Time: 536 ms (54.21%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,9 +1,4 @@
--- # Write your MySQL query statement below
--- SELECT name FROM Customer
--- WHERE referee_id is NULL OR referee_id != 2
-
-SELECT name FROM Customer
-WHERE IFNULL(referee_id , 0) <> 2;
-
-
--- <> is same as !=
+# Write your MySQL query statement below
+SELECT name
+FROM Customer
+WHERE referee_id != 2 OR referee_id IS NULL
